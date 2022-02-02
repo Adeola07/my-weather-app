db1d6395fb646e7fca949672eaf819fe8c01cafd
Adeola07 <88549439+Adeola07@users.noreply.github.com>
2022-02-02 12:40:29 +0000

    Update index.js
    Added forecast API

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ function formatDate() {
 }
 formatDate();
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
@@ -60,6 +61,12 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
 }
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "ced540f5728f0002c753875787475e3a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
   console.log(response.data);
@@ -82,6 +89,7 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 function showLocation(position) {
   let latitude = position.coords.latitude;
@@ -132,5 +140,5 @@ unitCelsius.addEventListener("click", changeCelsius);
 
 let unit = document.querySelector(".fahrenheit-temp");
 unit.addEventListener("click", changeFahrenheit);
-displayForecast();
+
 search("Sheffield");
